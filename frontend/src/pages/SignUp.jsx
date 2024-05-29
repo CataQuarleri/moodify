472c2450b09b4491815ce931dd4553ebf1743dc9
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-29 11:47:13 -0600

    add signup with form validation

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
 function SignUp() {
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   return (
-    <div>SignUp</div>
-  )
+    <div>
+      <h2>Signup</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email"> Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={FormData.email}
+            onChange={handleChange}
+            required
+          ></input>
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={FormData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <button type="submit">Signup </button>
+      </form>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
