7b81aa03dad7bc07f03d86ba6606992b244acdf6
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-29 14:38:22 -0600

    added a style to Sugnup.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,33 +4,41 @@ function SignUp() {
     console.log("Form submitted");
   };
   return (
-    <div>
-      <h2>Signup</h2>
+    <div className="max-w-md mx-auto">
+      <h2 className="text-3xl font-bold mb-4">Signup</h2>
       <form>
-        <div>
-          <label htmlFor="username">Username</label>
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700">
+            Username:
+          </label>
           <input
             type="text"
             id="username"
             name="username"
             onChange={handleChange}
             required
+            className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
           />
         </div>
 
-        <div>
-          <label htmlFor="email"> Email:</label>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700">
+            Email:
+          </label>
           <input
             type="email"
             id="email"
             name="email"
             onChange={handleChange}
             required
+            className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
           ></input>
         </div>
 
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700">
+            Password :
+          </label>
           <input
             type="password"
             id="password"
@@ -38,10 +46,16 @@ function SignUp() {
             value={FormData.password}
             onChange={handleChange}
             required
+            className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
           />
         </div>
 
-        <button type="submit">Signup </button>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Signup
+        </button>
       </form>
     </div>
   );
