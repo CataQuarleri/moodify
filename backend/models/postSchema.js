a59f13a30b8cbc5e09d4c04b7c4e519c0d1f22ea
Tabitha <tabitha.r.mccracken@gmail.com>
2024-05-29 11:31:00 -0600

    DB config and schemas

--- backend/models/postSchema.js
+++ backend/models/postSchema.js
@@ -1,2 +1,26 @@
 //reference to user (ID), each post has mood (emoji string) and color (hex)
 //check out populate on mongoose
+
+import mongoose from 'mongoose';
+
+const postsSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
+    mood: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: String,
+        required: false
+    }
+});
+
+
+export default mongoose.model(postsSchema);
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,1 +1,28 @@
 //email, password and username - 
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    user_id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    user_name: {
+        type: String,
+        required: true,
+        index: true
+    },
+    password: {
+        type: String,
+        required: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    }
+});
+
+export default mongoose.model('users', userSchema);
