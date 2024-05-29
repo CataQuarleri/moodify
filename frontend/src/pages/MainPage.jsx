09ba1f42e1b8987a893d2537838bd4c8f34bb491
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-29 14:40:57 -0600

    Add styling to add comment

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
+  return <div>MainPage</div>;
 }
 
-export default MainPage
+export default MainPage;
