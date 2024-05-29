6ac375a2e7aa2725576ffb9003e3562f093497e7
Agata D <124964478+Agata10@users.noreply.github.com>
2024-05-29 12:47:01 -0600

    Agata (#4)
    * Add tailwind CSS
    
    * Add navbar, delete styling, add style to page
    
    * Add our palette
    
    * Added Navbar
    
    * Add fonts
    
    ---------
    
    Co-authored-by: Cata Quarleri <91096792+CataQuarleri@users.noreply.github.com>

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          100: '#f689ab',
+        },
+        violet: {
+          100: '#a08cc4',
+        },
+        indigo: {
+          100: '#0e3e68',
+        },
+        turquoise: {
+          100: '#55cdad',
+        },
+        yellow: {
+          100: '#fec457',
+        },
+      },
+      fontFamily: {
+        logo: ['Kalam', 'cursive'],
+        primary: ['Montserrat', 'sans-serif'],
+      },
+    },
+    plugins: [],
+  },
+};
