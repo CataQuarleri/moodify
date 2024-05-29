4924d42813ba54d8c060264fed24f15c2f978fcb
Raven Alexander <theravenalexander@gmail.com>
2024-05-29 14:02:34 -0600

    adding auth functionality but missing d

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,26 +1,26 @@
 //reference to user (ID), each post has mood (emoji string) and color (hex)
 //
 import express from 'express'
-// import {getAllPosts, postNewPost, addCommenttoPost, seeAllCommentForOnePost, deleteOnePost} from '../controllers/postController.js'
+//import {getAllPosts, postNewPost, addCommenttoPost, seeAllCommentForOnePost, deleteOnePost} from '../controllers/postController.js'
 
 const router = express.Router()
 
 // grab all post 
-router.get('/', getAllPosts)
+//router.get('/', getAllPosts)
 
 // create new post
-router.post('/', postNewPost)
+//router.post('/', postNewPost)
 
 //add comment to post
 
-router.post('/:postId',addCommenttoPost )
+//router.post('/:postId',addCommenttoPost )
 
 // get all comments for 
 
-router.get('/:postId',seeAllCommentForOnePost )
+//router.get('/:postId',seeAllCommentForOnePost )
 
 //delete one post by post id
 
-router.delete('/:postId',deleteOnePost )
+//router.delete('/:postId',deleteOnePost )
 
 export default router
