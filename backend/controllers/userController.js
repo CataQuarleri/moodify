2eccdfc437873f5157ebc99f1f37678c96329ee2
Agata D <124964478+Agata10@users.noreply.github.com>
2024-05-29 15:16:11 -0600

    Agata (#19)
    * Add tailwind CSS
    
    * add signup with form validation
    
    * Add navbar, delete styling, add style to page
    
    * Add our palette
    
    * added a login page with form validation
    
    * Added Navbar
    
    * Add fonts
    
    * Add call to Action
    
    * Add responsive layout to navbar
    
    * Add Homepage
    
    * Add homepage caltoaction
    
    * Change font import
    
    * Add styling to add comment
    
    * Add controllers
    
    * Add main page
    
    ---------
    
    Co-authored-by: Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
    Co-authored-by: Cata Quarleri <91096792+CataQuarleri@users.noreply.github.com>

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,26 @@
-//
+const mongoose = require('mongoose');
+const user = require('../models/userSchema');
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await user.login(email, password);
+    res.status(200).json({ email });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
+  }
+};
+
+const userSignup = async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    const newUser = await user.signup({ userName, email, password });
+    res.status(200).json({ userName, email, password });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
+  }
+};
+
+module.exports = { userLogin, userSignup };
