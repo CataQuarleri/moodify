71db9dc816a9fc956d229accb9fb3a55b8cdc5af
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-29 12:11:37 -0600

    Add our palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          100: '#f689ab',
+        },
+        violet: {
+          100: '#a08cc4',
+        },
+        indigo: {
+          100: '#0e3e68',
+        },
+        turquoise: {
+          100: '#55cdad',
+        },
+        yellow: {
+          100: '#fec457',
+        },
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
